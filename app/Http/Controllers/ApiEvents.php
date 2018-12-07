502f1d2e3460595b7d2e89a0d4092c7831816fb7
FallenMaster <MASTER>
2018-12-08 00:41:26 +0300

    new routes for new site

---
+++ app/Http/Controllers/ApiEvents.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Event;
+
+class ApiEvents extends Controller {
+
+   public function ping() {
+      return 'Попингуй мне тут';
+   }
+
+   // Список ближайших мероприятий
+   public function get_index() {
+      $output['carousel'] = ApiEvents::get_carousel();
+      $output['events'] = ApiEvents::get_closest_events(5);
+      $output['exhibitions'] = ApiEvents::get_closest_exhibitions();
+      $output['news'] = ApiEvents::get_news();
+
+      return $output;
+   }
+   public function get_carousel() {
+      $slider = Event::where('right_column', '=', '1')
+         ->where('show_or_not', '=', '0')
+         ->where('date_to', '>=', date('Y-m-d'))
+         ->orderBy('date_from', 'ASC')
+         ->get();
+      return $slider;
+   }
+
+   // Список ближайших мероприятий
+   public function get_closest_events($quantity = 100) {
+      $closestEvents = Event::where('date_to', '>=', date('Y-m-d'))
+         ->where('tags', 'NOT LIKE', '%news%')
+         ->where('tags', 'NOT LIKE', '%exhibition%')
+         ->where('show_or_not', '0')
+         ->orderBy('date_from', 'asc')
+         ->take($quantity)
+         ->get(['id', 'title', 'date_from', 'date_to', 'what_time']);
+
+      return $closestEvents;
+   }
+
+   // Список ближайших конкурсов и выставок
+   public function get_closest_exhibitions() {
+      $exhibitions = Event::where('show_or_not', '=', '0')
+         ->where('date_to', '>=', date('Y-m-d'))
+         ->where('tags', 'LIKE', '%exhibition%')
+         ->orderBy('date_from', 'asc')
+         ->take(4)
+         ->get(['id', 'title', 'date_from', 'date_to', 'what_time']);
+
+      return $exhibitions;
+   }
+
+   // Список прошедших мероприятий по годам
+   public function get_past_events($year) {
+      $events = [];
+
+      for ($month = 12; $month >= 1; $month--){
+         $monthNames = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+         $monthEvents = Event::where('show_or_not', '=', '0')
+            ->where('date_to', '<', date('Y-m-d'))
+            ->whereYear('date_to', '=', $year)
+            ->whereMonth('date_to', '=', $month)
+            ->where('tags', 'NOT LIKE', '%news%')
+            ->orderBy('date_to', 'desc')
+            ->distinct()
+            ->get(['id', 'title', 'date_from', 'date_to', 'what_time']);
+
+            $ru_month = strftime("%B", mktime(0, 0, 0, $month, 12));
+            $events[$monthNames[$month - 1]] = $monthEvents;
+      }
+
+      return json_encode($events);
+   }
+
+   // Список пост-релизов конкурсов и выставок
+   public function get_news() {
+      $events = Event::where('post_reliz', '!=', '')
+         ->where('show_or_not', '0')
+         ->orderBy('date_to', 'desc')
+         ->skip(0)
+         ->take(12)
+         ->get(['id', 'title', 'date_from', 'date_to', 'what_time', 'post_reliz']);
+
+      return $events;
+   }
+
+   // Страница мероприятия
+   public function get_event($id) {
+      $event = Event::where('id', $id)
+         ->where('show_or_not', '=', '0')
+         ->first();
+
+      return $event;
+   }
+}
--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,14 @@ Route::get('/get_last_articles/{current_cat}/{current_id}', 'JournalArticles@get
 // Заявки в студии
 Route::post('/send_request_to_studio', 'Studios@sendRequestToStudio');
 Route::put('/finish_request_to_studio', 'Studios@finishRequestToStudio');
+
+// Новое апи
+Route::get('/ping', 'ApiEvents@ping');
+Route::get('/get_index', 'ApiEvents@get_index');
+Route::get('/get_carousel', 'ApiEvents@get_carousel');
+Route::get('/get_closest_events', 'ApiEvents@get_closest_events');
+Route::get('/get_closest_exhibitions', 'ApiEvents@get_closest_exhibitions');
+Route::get('/get_news', 'ApiEvents@get_news');
+// Мероприятия
+Route::get('/get_event/{id}', 'ApiEvents@get_event');
+Route::get('/get_past_events/{year}', 'ApiEvents@get_past_events');
